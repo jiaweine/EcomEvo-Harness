6bc24db115e5259c2828199bb0306a09c13d11f5
jwya <1817219421@qq.com>
2026-09-16 14:51:05 +0800

    feat: export policy-aware runtime engine

--- ecomevo/runtime/__init__.py
+++ ecomevo/runtime/__init__.py
@@ -1,9 +1,10 @@
-from .engine import EcomEvoEngine
+from .policy_engine import EcomEvoEngine
 from .event_store import EventStore
 from .planner import AdaptivePlanner
 from .verifier import DecisionVerifier
 from .sandbox import ActionSandbox
 from .tools import ToolRegistry, PTCExecutor
+from .policy_control import PolicyStore, PolicyVersion
 from .evolver import FailureDrivenEvolver
 from .autonomy import AutonomousController, TaskGraph
 from .skills import AdaptiveSkillLibrary
@@ -21,7 +22,7 @@ from .plugins import (
 
 __all__=[
     'EcomEvoEngine','EventStore','AdaptivePlanner','DecisionVerifier','ActionSandbox',
-    'ToolRegistry','PTCExecutor','FailureDrivenEvolver','AutonomousController','TaskGraph',
+    'ToolRegistry','PTCExecutor','PolicyStore','PolicyVersion','FailureDrivenEvolver','AutonomousController','TaskGraph',
     'AdaptiveSkillLibrary','AdaptiveDecisionPolicy','AdaptiveRoutingStore',
     'CounterfactualAdaptiveAutonomousController','CounterfactualAdaptiveDecisionPolicy',
     'HarnessEvolutionOptimizer','HarnessComponent','PluginRegistry','PluginDescriptor',
